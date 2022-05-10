1b35d3659fce3a57c0eaf3fdfebb67d18d85e476
anadege <anadege@student.42.fr>
2022-05-10 19:03:52 +0200

    ConfigModule set up to take into account .env variables for database directly from root directory

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,20 +7,17 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    //ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      envFilePath: '../.env',
+    }),
     UsersModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'randompass',
-      database: 'postgres',
-      //host: process.env.DATABASE_HOST,
-      //port: +process.env.DATABASE_PORT,
-      //username: process.env.DATABASE_USER,
-      //password: process.env.DATABASE_PASSWORD,
-      //database: process.env.DATABASE_NAME,
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true, //Load automatically entities without specifying the array
       synchronize: true // Synch DB with entities each time we load the app TODO disable when production
     }),
