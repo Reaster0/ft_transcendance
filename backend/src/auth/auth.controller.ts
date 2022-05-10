1a61af9837b0ed528645f16c1ad20e7704e53619
abaudot <aimebaudot@gmail.com>
2022-05-10 19:44:53 +0200

    end of day 1

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Auth42Guard } from './guards/auth42.guard';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+@Get('42/login')
+@UseGuards(Auth42Guard)
+login(){}
+}
--- backend/src/auth/guards/auth42.guard.ts
+++ backend/src/auth/guards/auth42.guard.ts
@@ -1,11 +1,5 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class Auth42Guard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
-  }
-}
+export class Auth42Guard extends AuthGuard('0Auth') {}
