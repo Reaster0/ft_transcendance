7ee41abd939801c1999e733ca9d80e40e7cb7feb
abaudot <aimebaudot@gmail.com>
2022-05-11 14:01:54 +0200

    begin

--- backend/src/auth/aut.strategy.ts
+++ backend/src/auth/aut.strategy.ts
@@ -2,5 +2,5 @@ import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import {Strategy} from "passport-42";
 
-//@Injectable()
-//export class auth42Strategy extends PassportStrategy(Strategy, '42'){}
+@Injectable()
+export class auth42Strategy extends PassportStrategy(Strategy, '42'){}
