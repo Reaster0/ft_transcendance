20be295259d6b62f2f6f6d0bbac443879367d8d7
abaudot <aimebaudot@gmail.com>
2022-05-13 17:24:45 +0200

    config redo (sorry)

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -6,24 +6,22 @@ import { User } from './entities/user.entity';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from 'src/auth/strategies/jwt.strategy';
 
 @Module({
 	imports: [
 		TypeOrmModule.forFeature([User]),
-		PassportModule,
+		PassportModule.register({defaultStrategy: 'jwt'}),
 		ConfigModule,
-		JwtModule.registerAsync({
-			imports: [ConfigModule],
-			inject: [ConfigService],
-			useFactory: async (configService: ConfigService) => ({
-				secret: configService.get('JWT_SECRET'),
-				signOptions: {
-					expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}s`,
-				},
+		JwtModule.register({ //or registerAsync
+			secret: process.env.JWT_SECRET,
+			signOptions: {
+				expiresIn: process.env.JWT_EXPIRATION,
+			},
 			}),
-		})],
+		],
 	controllers: [UsersController],
-	providers: [UsersService],
-	exports: [UsersService],
+	providers: [UsersService, JwtStrategy],
+	exports: [UsersService, JwtStrategy, PassportModule],
 })
 export class UsersModule {}
