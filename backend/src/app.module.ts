d1ddd2f087cca8948f436f87029042b957376c6b
abaudot <aimebaudot@gmail.com>
2022-05-13 17:24:45 +0200

    config redo (sorry)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,16 +4,17 @@ import { AppService } from './app.service';
 import { TypeOrmModule} from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
-import * as Joi from '@hapi/joi';
+import * as Joi from '@hapi/joi'; //remove 
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
-    UsersModule,
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client'),
+      rootPath: join(__dirname, '..', 'client'), //Need to ask nadege about this
     }),
+    ConfigModule.forRoot({isGlobal: true}),
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
         type: 'postgres',
@@ -26,18 +27,8 @@ import { join } from 'path';
         synchronize: true // Synch DB with entities each time we load the app TODO disable when production
       }),
     }),
-    ConfigModule.forRoot({
-      envFilePath: '../.env',
-      validationSchema: Joi.object({
-        DATABASE_HOST: Joi.string().required(),
-        DATABASE_PORT: Joi.number().default(5432),
-        DATABASE_USER: Joi.string().required(),
-        DATABASE_PASSWORD: Joi.string().required(),
-        DATABASE_NAME: Joi.string().required(),
-        JWT_SECRET: Joi.string().required(),
-        JWT_EXPIRATION_TIME: Joi.string().required(),
-      }),
-    }),
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
