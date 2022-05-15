b3d00729c278fe7b4792518a53d2a1ba463212a4
Reaster <earnaud@student.42.fr>
2022-05-15 03:30:49 +0200

    added a drawer and a top toolbar

---
+++ frontend/src/components/NavDrawer.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+	<v-toolbar flat dense app color="black">
+		<v-toolbar-title>
+			<v-btn icon @click="drawer = !drawer">
+				<v-icon>mdi-format-list-bulleted-square</v-icon>
+			</v-btn>
+
+			<router-link to="/" style="text-decoration: none; color: inherit;">
+				<span class="text-overline">Transcendance</span>
+			</router-link>
+
+		</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<router-link to="/signup">
+				<v-btn flat color="grey" align-end="true">
+					Login
+					<v-icon>mdi-login</v-icon>
+				</v-btn>
+		</router-link>
+	</v-toolbar>
+	<v-navigation-drawer v-model="drawer" temporary floating color="#2C3E50" background-color="#2C3E50">
+		<!-- put things there -->
+	</v-navigation-drawer>
+</div>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default {
+	setup(){
+		const drawer = ref(false);
+
+		return {drawer}
+	},
+}
+</script>
+
+<style>
+</style>
