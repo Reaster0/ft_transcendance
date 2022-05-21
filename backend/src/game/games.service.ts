4fa733a9e3a2a1d5f48989c4dbcc1288510e777c
anadege <anadege@student.42.fr>
2022-05-21 16:48:16 +0200

    game gateway and service progression

--- backend/src/game/games.service.ts
+++ backend/src/game/games.service.ts
@@ -2,30 +2,64 @@ import { Injectable } from '@nestjs/common';
 import { Socket } from 'socket.io';
 import { UsersService } from '../users/services/users.service';
 import { Match } from './interfaces/match.interface';
-
-let queue: Array<Socket> = new Array(); // Array of clients waiting for opponent
-let matchs: Array<Match> = new Array(); // Array of current match
+import { Player } from './interfaces/player.interface';
 
 @Injectable()
 export class GamesService {
 	constructor(
-		private readonly usersService: UsersService,
+		private readonly usersService: UsersService
 	) {}
 
-	joinQueue(client: Socket) {
-		queue.push(client);
-		if (queue.length >= 2) {
-			//Create and join match
+	isWaiting(client : Socket, queue: Array<Socket>): boolean {
+		if (queue.includes(client)) {
+			return true;
 		}
+		for (let sock of queue) {
+			if (sock.data.user.id == client.data.id) {
+				return true;
+			}
+		}
+		return false;
 	}
 
-	joinMatch(client: Socket, match: Match) {
-
+	isPlaying(client: Socket, matchs: Map<string, Match>): boolean {
+		for (let currMatch of matchs.values()) {
+			const matchPlayers = currMatch.players;
+			for (let currPlayer of matchPlayers) {
+				if (client == currPlayer.socket || client.data.user.id == currPlayer.user.id) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
+	setPlayer(client: Socket) : Player {
+		const player: Player = {
+			socket: client,
+			user: client.data.user,
+			score: 0,
+		}
+		return player;
+	}
 
+	setMatch(matchId: string, clients: Array<Socket>) : Match {
+		const matchPlayers: Array<Player> = new Array();
+		for (let client of clients) {
+			let newPlayer = this.setPlayer(client);
+			matchPlayers.push(newPlayer);
+		}
+		const match: Match = {
+			matchId: matchId,
+			players: matchPlayers,
+			watchers: new Array(),
+		};
+		return match;
+	}
 
-	watchMatch(client: Socket, match: Match) {
-
+	sendToPlayers(match: Match, toSend: string, ...args) {
+		for (let player of match.players) {
+			player.socket.emit(toSend, ...args); // emit more info ?
+		}
 	}
 }
--- backend/src/game/interfaces/match.interface.ts
+++ backend/src/game/interfaces/match.interface.ts
@@ -2,6 +2,7 @@ import { Player } from './player.interface';
 import { Socket } from 'socket.io';
 
 export interface Match {
+	matchId: string;
 	players: Array<Player>;
 	watchers: Array<Socket>;
 }
