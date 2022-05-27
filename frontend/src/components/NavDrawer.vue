6e19020b7b823f903769283866d5db869927837f
Reaster <earnaud@student.42.fr>
2022-05-27 16:23:21 +0200

    remove the unnescessary drawer

--- frontend/src/components/NavDrawer.vue
+++ frontend/src/components/NavDrawer.vue
@@ -1,15 +1,9 @@
 <template>
-<div>
 	<v-toolbar flat dense app color="black">
 		<v-toolbar-title>
-			<v-btn icon @click="drawer = !drawer">
-				<v-icon>mdi-format-list-bulleted-square</v-icon>
-			</v-btn>
-
 			<router-link to="/" style="text-decoration: none; color: inherit;">
 				<span class="text-overline">Transcendance</span>
 			</router-link>
-
 		</v-toolbar-title>
 			<v-spacer></v-spacer>
 				<v-btn v-if="!isLog" to="/login" flat color="grey" align-end="true">
@@ -24,11 +18,6 @@
 					<v-btn color="red" @click="logOut" to="/">Logout</v-btn>
 				</div>
 	</v-toolbar>
-	<v-navigation-drawer v-model="drawer" temporary floating color="#2C3E50" background-color="#2C3E50">
-		<!-- put things there -->
-		<v-card>nothing to see here</v-card>
-	</v-navigation-drawer>
-</div>
 </template>
 
 <script>
@@ -36,11 +25,6 @@ import { useStore } from 'vuex'
 import { computed } from 'vue'
 
 export default {
-	data() {
-		return {
-			drawer: false,
-		}
-	},
 	setup() {
 		const store = useStore();
 		
