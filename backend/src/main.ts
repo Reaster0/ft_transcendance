5647b55136068db5e6d21f47bb90ee73eecfd704
anadege <anadege@student.42.fr>
2022-05-29 21:01:21 +0200

    CI test again

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
+import { INestApplication } from '@nestjs/common';
+
+async function startServerCI(app: INestApplication) {
+  const promise = app.listen(3000);
+  if (process.argv.slice(2).indexOf("CI") !== -1) {
+    return new Promise(r => setTimeout(r, 10 *1000));
+  }
+  return promise;
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -25,6 +34,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  await app.listen(3000);
+  await startServerCI(app);
+  await app.close();
 }
 bootstrap();
