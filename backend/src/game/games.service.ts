7dbac301a61598e5f4228dae1d9a30607fc70434
anadege <anadege@student.42.fr>
2022-05-29 21:10:25 +0200

    test CI faulty

--- backend/src/game/games.service.ts
+++ backend/src/game/games.service.ts
@@ -295,6 +295,7 @@ export class GamesService {
       winner: match.winner.user.nickname,
     });
     server.socketsLeave(match.matchId);
+    thisshouldnotwork!
     // TODO set game inside DB
     matchs.delete(match.matchId);
   }
