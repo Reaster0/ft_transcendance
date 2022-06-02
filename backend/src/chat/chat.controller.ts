6d6fb7eac77b9429283f2500136f51a1e6ef04e1
abaudot <aimebaudot@gmail.com>
2022-06-02 16:19:53 +0200

    test public route

--- backend/src/chat/chat.controller.ts
+++ backend/src/chat/chat.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Post, Req } from "@nestjs/common";
+import { Request } from "express";
+import { RequestUser } from "src/auth/interfaces/requestUser.interface";
+import { CreateChannelDto } from "./chat.dto";
+import { ChanI } from "./interfaces/channel.interface";
+import { ChanServices } from "./services/chan.service";
+import { ChatServices } from "./services/chat.service";
 
 @Controller('chat') // localhost:3000/chat/....
 export class ChatController {
-    constructor() {}; //import service there
+    constructor(
+        private readonly chanService: ChanServices,
+    ){}
 
     @Post('protectedroom') //localhost:3000/chat/protectedroom
     protectedRoom(/*arguments*/){
@@ -15,8 +23,16 @@ export class ChatController {
     }
 
     @Post('publicroom') //localhost:3000/chat/publicroom
-    publicRoom(/*arguments*/){
-        //logic
+    async publicRoom(@Req() req: RequestUser, param: CreateChannelDto): Promise<Boolean> {
+       console.log(req); // jus cheking for test
+       let newChan : ChanI = {chanName: param.channame, publicChannel: true, password: ''}
+       try {
+        await this.chanService.createChannel(newChan, req.user)
+       } catch {
+           console.log('error while creating public channel');
+           return false;
+       }
+       return (true);
     }
 
 
---
+++ backend/src/chat/chat.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+
+
+export class CreateChannelDto {
+    @IsString()
+    @IsNotEmpty()
+    readonly channame: string
+
+    @IsAlphanumeric()
+    @IsOptional()
+    readonly password: string
+}
