b38d90bd30bb61b2285a9fc90af98f125cd1be6e
anadege <anadege@student.42.fr>
2022-06-02 11:55:29 +0200

    deletion of email

--- backend/src/users/user.dto.ts
+++ backend/src/users/user.dto.ts
@@ -2,33 +2,19 @@ import { ApiProperty } from '@nestjs/swagger';
 
 import {
   IsString,
-  IsEmail,
   IsNotEmpty,
   IsOptional,
   IsAlphanumeric,
   MinLength,
   MaxLength,
-  IsNumber
 } from 'class-validator';
 
 // DTO for user registration //
 export class CreateUserDto {
-  @ApiProperty({
-    type: String,
-    description:
-      'Private username for identifications. Will be stocked in background, but used as nickname in a first time.',
-  })
+  @ApiProperty({ type: String, description: 'Private username for identifications. Will be stocked in background, but used as nickname in a first time.' })
   @IsString()
   @IsNotEmpty()
   readonly username: string;
-
-  @ApiProperty({
-    type: String,
-    description: 'The user email address. Under email format.',
-  })
-  @IsEmail()
-  @IsNotEmpty()
-  readonly email: string;
 }
 
 // DTO for user modification //
@@ -39,9 +25,4 @@ export class UpdateUserDto {
   @MaxLength(15)
   @IsOptional()
   readonly nickname?: string;
-
-  @ApiProperty({ type: String, description: 'email' })
-  @IsEmail()
-  @IsOptional()
-  readonly email?: string;
 }
