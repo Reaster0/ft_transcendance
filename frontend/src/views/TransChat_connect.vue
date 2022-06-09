baf88db37ae5dcb65f74c7f44181c8f1bdb39e7a
anadege <anadege@student.42.fr>
2022-06-09 19:24:33 +0200

    TransChat_connect in TS

--- frontend/src/views/TransChat_connect.vue
+++ frontend/src/views/TransChat_connect.vue
@@ -38,51 +38,48 @@ for bloking or unblocking  a user:
 
 <script lang="ts">
 import { onMounted } from "@vue/runtime-core"
-import { ref } from "vue"
+import { ref, defineComponent } from "vue"
 import io from 'socket.io-client';
 //import { useKeypress } from "vue3-keypress";
 
-export default {
+export default defineComponent ({
     chanName: '' as string, 
     password: false as boolean, 
     publicChannel: false as boolean,
 
-	setup()
-    {
-		const connection = ref(null)
-		//const matchId = ref(null)
+	setup() {
+		const connection = ref<any | null>(null)
 
 		onMounted(() =>{
 			console.log(document.cookie.toString())
 			try {
 					connection.value = io('http://localhost:3000/chat',{
-					transportOptions: {
-					polling: { extraHeaders: { auth: document.cookie} },
-					withCredentials: true,
-					},
+						transportOptions: {
+						polling: { extraHeaders: { auth: document.cookie} },
+						withCredentials: true,
+						},
 				})
 				console.log("starting connection to websocket")
 			} catch (error) {
 				console.log("the error is:" + error)
 			}
 
-            // : connection.value.on(‘command’, (received) => {})
-            connection.value.on('channel', (channels) => 
-            {console.log("channel:" + channels)})
-
-			NewChannel();
-            TestTest();
-			SendingMessage();
-			JoinChannel();
-			LeaveChannel();
-			BlockUser();
+            connection.value.on('channel', (channels: string) => {
+				console.log("channel:" + channels)
 			})
+
+			//NewChannel();
+            //TestTest();
+			//SendingMessage();
+			//JoinChannel();
+			//LeaveChannel();
+			//BlockUser();
+		})
 
         // берет аргс и создает новый канал
         // for creating a new channel/room: 
 		// - createChannel { chanName: string, password:string, publicChannel: boolean }
-		function NewChannel(chanName, password, publicChannel)
-		{
+		function NewChannel(chanName: string, password: string, publicChannel: boolean) {
 			console.log("before createChannel");
 			connection.value.emit('createChannel', chanName, password, publicChannel);
 			console.log("after createChannel");
@@ -90,8 +87,7 @@ export default {
 
 		// for sending message:
 		// -  message {content: string, channel: Chan, ...}
-		function SendingMessage(content, channel)
-		{
+		function SendingMessage(content: string, channel: any) {
 			console.log("before message");
 			connection.value.emit('message', content, channel);
 			console.log("after message");
@@ -99,8 +95,7 @@ export default {
 
 		// for joinning a existing channel
 		// - joinChannel { id: string }
-		function JoinChannel(id)
-		{
+		function JoinChannel(id: string) {
 			console.log("before joinChannel");
 			connection.value.emit('joinChannel', id);
 			console.log("after joinChannel");
@@ -110,7 +105,7 @@ export default {
 
 		// for leaving channel:
 		// - leaveChannel { channel or id: string}
-		function LeaveChannel(channel)
+		function LeaveChannel(channel: string)
 		{
 			console.log("before leaveChannel");
 			connection.value.emit('leaveChannel', channel);
@@ -120,7 +115,7 @@ export default {
 
 		// for bloking or unblocking  a user:
 		// - blockUser{ user: User, block: boolean } // true => block false => unblock
-		function BlockUser(user, block)
+		function BlockUser(user: any, block: boolean)
 		{
 			console.log("before blockUser");
 			connection.value.emit('blockUser', user, block);
@@ -131,31 +126,15 @@ export default {
 		/// проверка открытах чатов по базе. автоматически подписать юзера на "основной чат"
 
         function TestTest(){
-			console.log("befor createChannel");
+			console.log("before createChannel");
 			connection.value.emit('createChannel');
 			console.log("after createChannel")
 		}
 
-
-
-
-
-
-        // useKeypress({
-		// keyEvent: "keydown",
-		// keyBinds:
-		// 	{
-		// 		keyCode: 13,
-		// 		success: () => {
-		// 			gameSocket.value.emit('sendMessage', {matchId: matchId.value, input: "Enter"})
-		// 		},
-		// 	},
-		// })
-
 		return {TestTest}
 
 	}
-}
+})
 </script>
 
 <style>
