f1b5c64355303a50f79bfd8ad8cf04d5cb4605c4
anadege <anadege@student.42.fr>
2022-06-09 18:38:04 +0200

    working version, only TheGame.vue set in ts

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,16 +1,16 @@
 <template>
-  <v-container>
-    <particles-bg type="cobweb" :bg="true" />
-    <v-img :aspect-ratio="16/9" height="500" src="../assets/42_Logo.svg"/>
-    <v-row>
-          <v-col align="center">
-          <h1 class="text">Play the</h1>
-          <v-btn elevation="24" outlined rounded min-height="100" min-width="200" color="rgb(255, 0, 0)" to="/game">
-            <h1 class="text rainbow glitch" data-text="Future">Game</h1>
-          </v-btn>
-          </v-col>
-  </v-row>
-  </v-container>
+	<v-container>
+		<particles-bg type="cobweb" :bg="true" />
+		<v-img :aspect-ratio="16/9" height="500" src="../assets/42_Logo.svg"/>
+		<v-row>
+			<v-col align="center">
+			<h1 class="text">Play the</h1>
+			<v-btn elevation="24" outlined rounded min-height="100" min-width="200" color="rgb(255, 0, 0)" to="/game">
+				<h1 class="text rainbow glitch" data-text="Future">Game</h1>
+				</v-btn>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
 
 <script lang="ts">
@@ -33,7 +33,7 @@ export default {
 <style lang="scss" scoped>
 .text{
 	font-size: 4em;
-  font-family: Spotnik;
+	font-family: Spotnik;
 	font-weight: bold;
 	color: white;
 	cursor: pointer;
@@ -48,15 +48,15 @@ text-shadow: 0.04em 0.04em #fc0049,
 }
 .glitch{
   color: white;
-  font-size:4em;
+  font-size: 4em;
   position:relative;
-  width: 5em;
+  width:5em;
   margin:0 auto;
 }
 @keyframes noise-anim{
   $steps:20;
   @for $i from 0 through $steps{
-    #{percentage($i* calc(1/$steps))}{
+    #{percentage($i*(1/$steps))}{
       clip:rect(random(100)+px,9999px,random(100)+px,0);
     }
   }
@@ -68,7 +68,7 @@ text-shadow: 0.04em 0.04em #fc0049,
   text-shadow:-1px 0 red;
   top:0;
   color:white;
-  background: red;
+  background:red;
   overflow:hidden;
   clip:rect(0,900px,0,0); 
   animation:noise-anim 2s infinite linear alternate-reverse;
@@ -76,7 +76,7 @@ text-shadow: 0.04em 0.04em #fc0049,
 @keyframes noise-anim-2{
   $steps:20;
   @for $i from 0 through $steps{
-    #{percentage($i* calc(1/$steps))}{
+    #{percentage($i*(1/$steps))}{
       clip:rect(random(100)+px,9999px,random(100)+px,0);
     }
   }
@@ -88,7 +88,7 @@ text-shadow: 0.04em 0.04em #fc0049,
   text-shadow:1px 0 blue; 
   top:0;
   color:white;
-  background:black;
+  background:red;
   overflow:hidden;
   clip:rect(0,900px,0,0); 
   animation:noise-anim-2 3s infinite linear alternate-reverse;
