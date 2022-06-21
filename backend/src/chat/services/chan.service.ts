1bd01a1a7a38f663591acb173bd9173edeb4ba1f
abaudot <aimebaudot@gmail.com>
2022-06-21 18:17:26 +0200

    leave correction

--- backend/src/chat/services/chan.service.ts
+++ backend/src/chat/services/chan.service.ts
@@ -69,14 +69,17 @@ export class ChanServices {
 
   async removeUserFromChan(channelId: string, user: User): Promise<Channel> {
     let update: Channel = await this.chanRepository.findOne(channelId, { relations : ['users'] });
-    const index = update.users.indexOf(user);
-    if (index == -1) {
-      return null;
+
+
+    for (const [i, value] of update.users.entries()) {
+      if (value.id === user.id) {
+        update.users.splice(i, 1);
+        break ;
+      }
     }
-    update.users.splice(index, 1);
-    await this.chanRepository.update(channelId, update);
+    
+    await this.chanRepository.save(update);
     const chanUser = await this.roleRepository.findOne({ where: { channel: update, userId: user.id} });
-    console.log(chanUser);
     try {
       this.roleRepository.remove(chanUser);
     } catch (err) {
